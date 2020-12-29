c6e0f913bed1c75d52de1b38884576d98fb19509
= <venukumarmaddela@gmail.com>
2020-12-29 22:48:28 +0530

    Print Numbers

---
+++ src/masters/in/code/basics/PrintNumbers.java
@@ -0,0 +1,13 @@
+package masters.in.code.basics;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++) {
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
